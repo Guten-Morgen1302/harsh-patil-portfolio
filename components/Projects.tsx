6199eceb35509fe1136178f55ad314e785dca317
callmeson <99388058+Guten-Morgen1302@users.noreply.github.com>
2025-03-06 20:38:55 +0530

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -53,7 +53,7 @@ const projects = [
     image:
       "https://images.unsplash.com/photo-1555421689-3f034debb7a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
     technologies: ["Next.js", "MongoDB", "Tailwind CSS"],
-    liveDemo: "#",
+    liveDemo: "https://cook-with-us.vercel.app/",
     github: "https://github.com/Guten-Morgen1302/Recipe-Sharing-Platform",
   },
 ]
